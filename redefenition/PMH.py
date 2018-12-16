a88866419c1f3c5a4ca74fdff2bf7ecac2273393
Silentdesu <silentdesu@gmail.com>
2018-12-17 00:22:46 +0800

    Some changes

--- redefenition/PMH.py
+++ redefenition/PMH.py
@@ -1,11 +1,11 @@
 from Man import *
 from Hombre import *
 
-osas = Man('Osas')
+osas = Man('Osas') #Создаем экземпляр/переменную и присваиваем класс Man с атрибутом имени
 fuhrer = Hombre('Fuhrer')
 
-osas.speak('it\'s a beautiful evening:\n')
+osas.speak('it\'s a beautiful evening:\n')#Вызываем у класса Man метод speak('обязательный текст')
 fuhrer.speak('Es una tarde hermosa.\n')
 
-Person.speak(osas)
+Person.speak(osas)#Вызываем главный класс и его метод speak(готовый или можно создать новый экземпляр,msg(В нем уже есть по-дефолту сообщение,но можно и написать свое))
 Person.speak(fuhrer)
